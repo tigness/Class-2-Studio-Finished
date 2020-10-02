bf8f14ccb3be41395089ba4c93faef02f143c066
Monica C <tigina18@gmail.com>
2020-10-01 19:06:17 -0500

    Add project files.

---
+++ Class 2 Studio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Class_2_Studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
